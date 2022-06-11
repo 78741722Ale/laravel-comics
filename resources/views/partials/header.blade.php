9bdc278642726d63035676f21683ff3c2274bc98
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-11 17:54:26 +0200

    Sistemato dimensioni errate e recuperate tramite figma

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,25 +1,31 @@
 <div class="flex_cent">
     <!-- Site Additional -->
     <div class="flex_cent" id="site_additional">
-        <div class="row w_90 flex_end m-0 h-100">
-            <!-- Col for Power$Additional -->
-            <div class="col-2 flex_cent_2 bg-danger h-100">Power Visa</div>
-            <div class="col-2 flex_cent_2 bg-danger h-100">Additional</div>
+        <div class="container-xl h-100">
+            <div class="row w-100 flex_end m-0 h-100">
+                <!-- Col for Power$Additional -->
+                <div class="col-2 flex_cent_2 bg-danger h-100">Power Visa</div>
+                <div class="col-2 flex_cent_2 bg-danger h-100">Additional</div>
+            </div>
         </div>
     </div>
     <!-- Site Navbar -->
     <div class="flex_cent" id="site_navbar">
-        <div class="row w_90 flex_bet m-0 h-100">
-            <!-- Col for Logo&Navbar&Searchbar-->
-            <div class="col-2 flex_cent_2 bg-warning h-100">logo</div>
-            <div class="col-7 flex_cent_2 bg-warning h-100">Navbar</div>
-            <div class="col-2 flex_cent_2 bg-warning h-100">Searchbar</div>
+        <div class="container-xl h-100">
+            <div class="row w-100 flex_bet m-0 h-100">
+                <!-- Col for Logo&Navbar&Searchbar-->
+                <div class="col-2 flex_cent_2 bg-warning h-100">logo</div>
+                <div class="col-7 flex_cent_2 bg-warning h-100">Navbar</div>
+                <div class="col-2 flex_cent_2 bg-warning h-100">Searchbar</div>
+            </div>
         </div>
     </div>
     <!-- Jumbotron -->
     <div class="flex_cent" id="site_jumbo">
-        <div class="row w_90 m-0 flex_cent_2 h-100">
-            Jumbotron Goes Here
+        <div class="container-xl h-100">
+            <div class="row w-100 m-0 flex_cent_2 h-100">
+                Jumbotron Goes Here
+            </div>
         </div>
     </div>
 </div>
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -3,13 +3,13 @@
     <!-- Site Cards -->
     <div class="flex_cent" id="site_card">
         <div class="container-xl h-100">
-            <div class="w-100 bordo p-5 flex_around h-100 flex-wrap">
+            <div class="w-100 p-5 flex_around h-100 flex-wrap">
                 <!-- Cards - Falle poi dinamiche (OK TIENILE COSI) -->
-                <div class="card mx-3 p-0" style="width: 13.5rem;">
+                <div class="card mx-3 p-0" style="width: 11rem;">
                     <!-- Immagine -->
                     <img src="https://picsum.photos/200" class="card-img-top" alt="Testo Dinamico">
                     <!-- Text  -->
-                    <div class="card-body p-0 pt-2">
+                    <div class="card-body p-0 pt-1">
                         <h5 class="card-title">Card title</h5>
                     </div>
                 </div>
@@ -19,7 +19,7 @@
     <!-- Site Icons -->
     <div class="flex_cent" id="site_icon">
         <div class="container-xl h-100">
-            <div class="row bordo flex_bet w-100 m-0 h-100">
+            <div class="row flex_bet w-100 m-0 h-100">
                 <!-- 1 Col (Sono dinamiche)-->
                 <div class="col-2 mx-3 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
                 <!-- 2 Col -->
