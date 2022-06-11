7e1ef5262bfbe481161318b6b726bfe4a07295b1
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-11 11:53:13 +0200

    Conclusa Struttura a Blocchi

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -2,21 +2,39 @@
 <div class="flex_cent">
     <!-- Site Cards -->
     <div class="flex_cent" id="site_card">
-        <div class="row w_90 m-0 h-100 bordo">
-            SiteCards Goes Here
+        <!-- 1° ROW -->
+        <div class="bordo w_90 p-5 flex_around h-100 flex-wrap">
+            <!-- Cards - Falle poi dinamiche (OK TIENILE COSI) -->
+            <div class="card mx-3 p-0" style="width: 13.5rem;">
+                <!-- Immagine -->
+                <img src="https://picsum.photos/200" class="card-img-top" alt="Testo Dinamico">
+                <!-- Text  -->
+                <div class="card-body p-0 pt-2">
+                    <h5 class="card-title">Card title</h5>
+                </div>
+            </div>
         </div>
     </div>
     <!-- Site Icons -->
     <div class="flex_cent" id="site_icon">
-        <div class="row w_90 m-0 h-100 bordo">
-            Site Icons Goes Here
+        <div class="row flex_around w_90 m-0 h-100 bordo">
+            <!-- 1 Col (Sono dinamiche)-->
+            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <!-- 2 Col -->
+            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <!-- 3 Col -->
+            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <!-- 4 Col -->
+            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <!-- 5 Col -->
+            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
         </div>
     </div>
 </div>
 
 
 <!-- Prova titoli -->
-   <!--  @forelse ($comics as $comic)
+    <!--  @forelse ($comics as $comic)
         <h1>{{$comic['title']}}</h1>
         @empty
         <p>Vuoto</p>
