389b3d5576aa34397e2dd651778dc4fd4e4aa921
78741722Ale <pecorillaalessandro@gmail.com>
2022-06-11 12:40:55 +0200

    Aggiunto contenuto e Classi

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,20 +2,20 @@
 <div class="flex_cent">
     <!-- Site Cards -->
     <div class="flex_cent" id="footer_upper">
-        <div class="row flex_bet w_90 m-0 h-100 bordo">
+        <div class="row flex_bet w_90 m-0 h-100">
             <!-- List Items -->
-            <div class="col-4 h-100 bordo flex_cent_2 bg-secondary">List Goes Here</div>
+            <div class="col-4 h-100 flex_cent_2 bg-secondary">List Goes Here</div>
             <!-- Logo -->
-            <div class="col-5 h-100 bordo flex_cent_2 bg-success">DC Logo Goes Here</div>
+            <div class="col-5 h-100 flex_cent_2 bg-success">DC Logo Goes Here</div>
         </div>
     </div>
     <!-- Site Icons -->
     <div class="flex_cent" id="footer_under">
-        <div class="row w_90 flex_bet m-0 h-100 bordo">
+        <div class="row w_90 flex_bet m-0 h-100">
             <!-- Sign Up Buttons -->
-            <div class="col-2 h-100 bordo bg-warning">Sign Up Goes Here</div>
+            <div class="col-2 h-100 bg-warning flex_cent_2">Sign Up Goes Here</div>
             <!-- Follow Up Buttons -->
-            <div class="col-2 h-100 bordo bg-warning">Follow Up Goes Here</div>
+            <div class="col-2 h-100 bg-warning flex_cent_2">Follow Up Goes Here</div>
         </div>
     </div>
 </div>
--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -3,7 +3,7 @@
     <!-- Site Cards -->
     <div class="flex_cent" id="site_card">
         <!-- 1° ROW -->
-        <div class="bordo w_90 p-5 flex_around h-100 flex-wrap">
+        <div class="w_90 p-5 flex_around h-100 flex-wrap">
             <!-- Cards - Falle poi dinamiche (OK TIENILE COSI) -->
             <div class="card mx-3 p-0" style="width: 13.5rem;">
                 <!-- Immagine -->
@@ -17,17 +17,17 @@
     </div>
     <!-- Site Icons -->
     <div class="flex_cent" id="site_icon">
-        <div class="row flex_around w_90 m-0 h-100 bordo">
+        <div class="row flex_around w_90 m-0 h-100">
             <!-- 1 Col (Sono dinamiche)-->
-            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <div class="col-2 mx-4 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
             <!-- 2 Col -->
-            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <div class="col-2 mx-4 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
             <!-- 3 Col -->
-            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <div class="col-2 mx-4 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
             <!-- 4 Col -->
-            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <div class="col-2 mx-4 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
             <!-- 5 Col -->
-            <div class="col-2 mx-4 bg-danger h-100 bordo"></div>
+            <div class="col-2 mx-4 bg-danger h-100 flex_cent_2">Icons Goes Here</div>
         </div>
     </div>
 </div>
